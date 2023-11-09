9ac257b71860234a5d92e9b0d6c9f9d065a163e2
Whillian-Sousa <whillscf@gmail.com>
2023-11-09 01:43:53 -0300

    first commit

---
+++ app/(root)/communities/page.tsx
@@ -0,0 +1,12 @@
+
+const Page = async () => {
+  return (
+    <section className="">
+      <h1 className="head-text mb-10">
+        communities
+      </h1>
+    </section>
+  )
+}
+
+export default Page
